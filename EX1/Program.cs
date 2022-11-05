2d31372c5f5682ecd32b0452f93cc7d90b7ffe7a
alya <alya-892@yandex.ru>
2022-11-06 01:12:32 +0300

    Задача1

---
+++ EX1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое число");
+int A =Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int B= Convert.ToInt32(Console.ReadLine());
+int S=1;
+for( int k=1; k<=B; k++)
+{
+S=S*A;
+}
+Console.WriteLine(S);
