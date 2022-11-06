4d81ad57857b9ddfa8624232f2ec4b932bbbb86b
alya <alya-892@yandex.ru>
2022-11-06 15:53:30 +0300

    Задача3

--- EX3/Program.cs
+++ EX3/Program.cs
@@ -3,6 +3,6 @@ int N= Convert.ToInt32(Console.ReadLine());
 int [] A= new int[N];
 for( int i=0; i<A.Length;i++)
 {
-    A[i]= new Random().Next(1,100);
+    A[i]= new Random().Next(1,10);
     Console.Write(A[i] +" ");
 }
