374f645032997a0d8679230c1e3a23769b09d034
alya <alya-892@yandex.ru>
2022-11-06 15:46:30 +0300

    Задача3

--- EX2/Program.cs
+++ EX2/Program.cs
@@ -1,11 +1,11 @@
-﻿Console.WriteLine("Введите  число"); 
-int B= Convert.ToInt32(Console.ReadLine());
-string A =B.ToString(); 
-int S=0, k=0;
-for( int i=0;i<A.Length;i++)
-{
-    k=B%10;// остаток
-    S=S+k;
-    B=B/10;
-    }
+﻿Console.WriteLine("Введите  число");  
+int B= Convert.ToInt32(Console.ReadLine()); 
+string A =B.ToString();  
+int S=0, k=0; 
+for( int i=0;i<A.Length;i++) 
+{ 
+    k=B%10;// остаток 
+    S=S+k; 
+    B=B/10; 
+    } 
 Console.WriteLine(S);
---
+++ EX3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите кол-во элементов");
+int N= Convert.ToInt32(Console.ReadLine());
+int [] A= new int[N];
+for( int i=0; i<A.Length;i++)
+{
+    A[i]= new Random().Next(1,100);
+    Console.Write(A[i] +" ");
+}
